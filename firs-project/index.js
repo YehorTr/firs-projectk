402c52de397d46bd795202842b20b1fd1da798ed
YehorTr <egortrachuk21@gmail.com>
2025-05-04 19:07:24 +0300

    Add files via upload

---
+++ firs-project/index.js
@@ -0,0 +1,16 @@
+// console.log('Hello Word 1');
+// console.log('Hello Word 2');
+// console.log('Hello Word 3');
+
+// alert("Hello world!");
+// alert(" 123");
+
+// prompt("Сколько Вам лет?", "18+");
+ console.log();
+ alert('5')
+ alert('4')
+ alert('3')
+ alert('2')
+ alert('1')
+ alert('0')
+ alert("Обратный отсчет закончен")
---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Бренданом Эйхом')
+alert('10')
+alert('1995')
+alert('JavaScript — это не Java')
+alert('ECMAScript')
