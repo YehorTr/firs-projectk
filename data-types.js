79e1bbf7ecb4d39cc182a585728fe949e0dbfa26
YehorTr <egortrachuk21@gmail.com>
2025-05-07 12:30:52 +0300

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,68 @@
+// String
+// const favoreDrink='Coffee';
+// console.log(favoreDrink);
+
+// // number
+// const numberOfCups = 10;
+// console.log(numberOfCups);
+
+// // Boolesn
+// const isColdDrink = true;
+// console.log(isColdDrink);
+
+// //  null пусто,значение не известно
+// const studeintFovoriteDrink = null;
+// console.log(studeintFovoriteDrink);
+ 
+// // undefined 
+// let carOwener;
+// console.log(carOwener);
+
+// // object
+// const drink ={
+//     favoreDrink: 'Coffee',
+//     numberOfCups: 5,
+//     isColdDrink: true,
+//  };
+// console.log(drink);
+
+// // symbol
+//  const id = Symbol('id');
+//  console.log(id);
+
+// //  bigint большие числа
+//  const binIntNumber = BigInt(10);
+//  console.log(binIntNumber);
+
+const age = 31;
+console.log(age);
+console.log(typeof 31);
+
+const name = 'Yehor';
+console.log(name);
+console.log(typeof name);
+
+const isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+
+const voter =null;
+console.log(voter);
+console.log(typeof voter);
+
+const notAssigned = undefined;
+console.log(notAssigned);
+console.log(typeof notAssigned);
+
+const person ={name: 'Anna', age: 25, isStudent:true };
+console.log(person);
+console.log(typeof person);
+
+const bigNumber = 125n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+const id = Symbol('id');
+console.log(id);
+console.log(typeof id);
